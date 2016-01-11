ad120bd64188bf44214f251b958fcebdb1d9943a
Amit K. Chauhan <kumar352@gmail.com>
2016-01-11 21:12:04 +0530

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Skeleton
+  class Application < Rails::Application
+    # custom path for rails
+    config.paths["app/controllers"] << "app/rails/controllers"
+    config.paths["app/models"] << "app/rails/models"
+    config.paths["app/views"] << "app/rails/views"
+    config.paths["app/helpers"] << "app/rails/helpers"
+    config.paths["app/assets"] << "app/static"
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
